9bc48d1e80e325ca0b18cbc1b7b627e5bf4d3f39
WW-SWZ <zhaojian0079527@163.com>
2025-04-09 19:54:15 +0800

    Upgrade Boot & Cloud

--- src/main/java/charles/zhou/serviceconsumer/ServiceConsumerApplication.java
+++ src/main/java/charles/zhou/serviceconsumer/ServiceConsumerApplication.java
@@ -2,11 +2,11 @@ package charles.zhou.serviceconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableFeignClients
 public class ServiceConsumerApplication {
     public static void main(String[] args) {
